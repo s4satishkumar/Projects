be5d4c7fd506b4fdb21580766e74383a7d932727
s4satishkumar <satishkumar8210770481@gmail.com>
2022-09-07 18:41:52 +0530

    category GET getbyid, GET getbyname PUT updatedbyid
    all these things have no command including only npm start and the rest is coded in services. categry and controller. category and routes that it and after that all work done by postman by fetching the PUT, GET.

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const categoryRoutes = require('./routes/category.routes');
-
-// let serverConfigObject= require('./config/serverConfig')
-// console.log(serverConfigObject)
-// const PORT =3000;
-// it is exporting port as object and print as object
-
-
-const { PORT} = require('./config/serverConfig');
-// we can  print only port like this 
-
+// const productRoutes = require('./routes/product.routes');
+const {PORT} =  require('./config/serverConfig');
 const app = express();
 
 /* app.use() is using the provided middleware for every incoming request to the server*/
 /* We need to a body parser middleware, that will help express to read all the query and body params */
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 categoryRoutes(app);
-console.log(PORT);
-app.listen(PORT, () =>{
-  console.log( 'server is listening to prot :',PORT);
+
+// productRoutes(app);
+
+app.listen(PORT, ()=>{
+    console.log('server is listening to port: ', PORT);
 });
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.get('/home', (req, res) => {
-//   res.send('welcome to homepage')
-// })
-
-// app.get('/', (req, res) => {
-//     res.send('root')
-//   })
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,15 +1,23 @@
 const CategoryController = require('../controllers/category.controller');
 
-const routes = (app) =>{
+const routes = (app) => {
     /* to get all the categories */
     app.get('/ecomm/api/v1/categories', CategoryController.getCategories);
 
-   /* to create a new category */
-app.post('/ecomm/api/v1/categories', CategoryController.createCategory);
+    /* to create a new category */
+    app.post('/ecomm/api/v1/categories', CategoryController.createCategory)
+
+    /* to get all categories by id */
+    app.get('/ecomm/api/v1/categories/:id', CategoryController.getCategoriesById);
+
+    /* to get all categories by name */
+    app.get('/ecomm/api/v1/categoriesByName/', CategoryController.getCategoriesByName);
+
+    /* to update the category by given id */
+    app.put('/ecomm/api/v1/categories/:id', CategoryController.updateCategory);
+
+    /* to delete a category by id*/
+    app.delete('/ecomm/api/v1/categories/:id', CategoryController.deleteCategory)
 }
 
-
-// to export one thing 
-// module.exports = routes;
 module.exports = routes;
-
