9f85b6ae6fc1f74b84810c14cc240228f6d39f33
s4satishkumar <satishkumar8210770481@gmail.com>
2022-09-21 19:38:42 +0530

    JWT Token Added , Authentication DONE

--- ecommerce/services/auth.service.js
+++ ecommerce/services/auth.service.js
@@ -1,5 +1,17 @@
 const {User} = require('../models/index');
 const bcrypt=require('bcryptjs');
+
+
+// var jwt = require('jsonwebtoken');
+
+//generate token
+// var token =jwt.sign({ email:'s4satishkumar@gmail.com', password:'s4satishkumar'}, 'my-secret-key');
+// console.log(token);
+
+// //verify token 
+// const decoded =jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InM0c2F0aXNoa3VtYXJAZ21haWwuY29tIiwicGFzc3dvcmQiOiJzNHNhdGlzaGt1bWFyIiwiaWF0IjoxNjYzNzY3MzU2fQ.YFxUYWoDdc2fmVZj4zO2JX_PnXfM_qwqcDL-PxWUezc','my-secret-key' );
+// console.log(decoded);
+
 const signup = (data) =>{
     const response = User.create({
         username: data.username,
