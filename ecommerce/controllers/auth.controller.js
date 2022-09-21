ca9ac6f8b6e56c5aff1bf334e829e24df8af6b50
s4satishkumar <satishkumar8210770481@gmail.com>
2022-09-22 02:12:14 +0530

    Category added by authorization

--- ecommerce/controllers/auth.controller.js
+++ ecommerce/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const authService = require('../services/auth.service');
+var jwt = require('jsonwebtoken');
 
 const signup = async(req, res) =>{
     const response = await authService.signup(req.body);
@@ -27,12 +28,12 @@ else{//user is present in the db for given email id
     const passwordVerifed =authService.verifyPassword(req.body.password, userData.password);
 
     if(passwordVerifed){
-
+        var token =jwt.sign({ email:userData.email, password:userData.password, username:userData.username}, process.env.JWT_SECRET_KEY);
         return res.json({ 
             message: 'signed in successfully',
             success: true,
-            data: userData,
             code: 200,
+            token:token
         });
     }
     else{ // password is incorrect
